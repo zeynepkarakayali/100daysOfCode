bc50a3ed93fdc0d5c065c19e20c2a48fa1f2a9e6
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-05 21:03:53 +0300

    modified the file

--- day1.5.py
+++ day1.5.py
@@ -1,5 +1,6 @@
-# BAND NAME GENERATOR
 #Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+print("Hello! Welcome to the Band Name Generator! ")
 
 color = input("Please write your favorite color. ")
 animal = input("The animal you want to be the most? ")
