63fd4f5cd433eb281128e5793d8c2c6d1305b257
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-05 21:01:05 +0300

    files added

---
+++ day1.5.py
@@ -0,0 +1,8 @@
+# BAND NAME GENERATOR
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+color = input("Please write your favorite color. ")
+animal = input("The animal you want to be the most? ")
+zodiac = input("What is your zodiac sign? ")
+
+print("The name of your band is: " + zodiac + " " + color + " " + animal)
