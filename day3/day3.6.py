663b56b83c4c08bb76e677559e2a641e68b3639e
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-07 12:11:52 +0300

    added file

---
+++ day3/day3.6.py
@@ -0,0 +1,54 @@
+# TREASURE ISLAND GAME
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("You are walking in an echanted forest and come into a cross road.")
+road = input("Which way are you going to choose? Left or Right? ").lower()
+
+if(road == "right"):
+    print("The eagles attacked you. Game Over.")
+elif(road == "left"):
+    print("You are lucky. You may pass.")
+
+    print("Now you arrived at a dark grey pond.")
+    swim = input('Are you going to "swim" or are you going to "wait?" ').lower()
+
+    if(swim == "swim"):
+        print("The piranhas attacked you. You died. Game Over.")
+    else:
+        print("A boat arrived! You crossed the lake unharmed.")
+        door = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ').lower()
+
+        if(door == "yellow"):
+            print("YOU WON THE GAME!!!")
+        else:
+            print("You lost try again. :(")
+
+else:
+    print("There are no roads like that.")
