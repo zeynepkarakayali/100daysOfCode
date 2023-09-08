5a67fd919c9b60287eb038ec44d607e007c38443
Zeynep Karakayalı <zeynep.karakayali@sabancidx.com>
2023-09-08 13:44:32 +0300

    added file

---
+++ day4/day4.2.py
@@ -0,0 +1,5 @@
+"""
+You are going to write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+Important: You are not allowed to use the choice() function.
+Line 8 splits the string names_string into individual names and puts them inside a List called names. For this to work, you must enter all the names as names followed by comma then space. e.g. name, name, name
+"""
