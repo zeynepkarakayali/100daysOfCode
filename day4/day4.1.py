0e7b0a65d75bf6ab33675e3ccbac74687fdeb343
Zeynep Karakayalı <zeynep.karakayali@sabancidx.com>
2023-09-08 11:57:12 +0300

    added file

---
+++ day4/day4.1.py
@@ -0,0 +1,18 @@
+"""
+You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+Important, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
+There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails.
+e.g. 1 means Heads 0 means Tails
+"""
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+	 
+#Write the rest of your code below this line 👇
+coin = random.randint(0,1)
+#print(coin)
+if(coin == 0):
+    print("Tails")
+elif(coin==1):
+    print("Heads")
