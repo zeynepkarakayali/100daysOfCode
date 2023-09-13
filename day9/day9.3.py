5dd03b7718a26c9402a0c4bb50f83bb287bd6d6d
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-13 23:17:08 +0300

    made changes

--- day9/day9.3.py
+++ day9/day9.3.py
@@ -1,2 +1,29 @@
-from replit import clear
-#HINT: You can call clear() to clear the output in the console.
+# The (not so) Secret Auction Program
+
+from art import logo
+
+def auction(bidders):
+
+    highestBid = 0
+    highestBidder = ""
+
+    for bidder in bidders:
+        if(bidders[bidder] > highestBid):
+            highestBid = bidders[bidder]
+            highestBidder = bidder
+    print(f"The winner of the auction is: {highestBidder} with a bid of ${highestBid}")
+
+bidders = {}
+biddingFinished = False
+while not biddingFinished:
+    print("Welcome to the secret auction program. ")
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? "))
+    continuee = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+    bidders[name] = bid
+
+    if continuee == "no":
+        auction(bidders)
+        biddingFinished = True
+    elif continuee == "yes":
+        auction(bidders)
