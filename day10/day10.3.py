72285537674b54f86cedfd5490e39dd8800a5fe5
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-14 14:15:16 +0300

    made changes

--- day10/day10.3.py
+++ day10/day10.3.py
@@ -1,3 +1,5 @@
+from art import logo
+
 # ADD
 def add(n1, n2):
     return n1 + n2
@@ -21,16 +23,34 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number? : "))
-
+print("Hello, Welcome to the calculator! ")
 print("The operations you can execute with the calculator are: ")
-
 for i in operations:
     print(i)
-choice = input("Which operation do you want to execute? Pick from the operations above: ")
 
-num2 = int(input("What's the second number? : "))
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? : "))
 
-func = operations[choice]
-result = func(num1, num2)
-print(f"{num1} {choice} {num2} = {result}")
+    operationFinished = False
+    while not operationFinished:
+
+        choice = input("Pick an operation: ")
+        func = operations[choice]
+
+        num2 = float(input("What's the second number? : "))
+
+        result = func(num1, num2)
+        print(f"{num1} {choice} {num2} = {result}")
+
+        continuation = input(f"Type 'y' to continue calculating with {result} ,type 'n' to start a new calculation, or type 'e' to exit. ").lower()
+        if(continuation == "y"):
+            operationFinished = False
+            num1 = result
+        elif(continuation == "n"):
+            operationFinished = True
+            calculator()
+        elif(continuation == "e"):
+            operationFinished = True
+
+calculator()
