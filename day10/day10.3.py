2c6bfa7c81b3e31798a11a33b3923984f48707a8
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-14 13:10:40 +0300

    added files

---
+++ day10/day10.3.py
@@ -0,0 +1,15 @@
+# ADD
+def add(n1, n2):
+    return n1 + n2
+
+# SUBTRACT
+def subtract(n1, n2):
+    return n1 - n2
+
+# MULTIPLY
+def multiply(n1, n2):
+    return n1 * n2
+
+#DIVIDE
+def divide(n1, n2):
+    return n1 / n2
