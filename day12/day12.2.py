4f92286c8e1e3418a9f6a249eac8f38b507c2944
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-18 11:10:52 +0300

    made changes

--- day12/day12.2.py
+++ day12/day12.2.py
@@ -1,1 +1,31 @@
 # NUMBER GUESSING GAME
+import random
+
+print("Welcome to the number guessing game! ")
+print("I am going to think of a number between 1 and 100. Try to guess it! ")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+attempt = 10
+if(difficulty == 'hard'):
+    attempt = 5
+
+number = random.randint(0,100)
+print(f"pstt, the number is: {number}. Act like you don't know it. ")
+
+guess = 101
+while (attempt > 0) and (guess != number):
+    print(f"You have {attempt} attempts remaining to guess the number. ")
+    guess = int(input("Make a guess: "))
+    if(guess > number):
+        print("Too high.")
+        print("Guess again.")
+    elif(guess < number):
+        print("Too low.")
+        print("Guess again.")
+    else:
+        print("Congrats! You have found the number.")
+        print("Are you a magician :d")
+    attempt -= 1
+
+if(attempt == 0):
+    print("You ran out of attempts. :(")
