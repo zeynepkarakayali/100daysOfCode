7709334e6c3e3f6e1b722dbd09f2f4285741c4ec
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-24 00:44:22 +0300

    made changes

--- day16/main.py
+++ day16/main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+moneyMachine = MoneyMachine()
+coffeeMaker = CoffeeMaker()
+isOver = False
+
+while not isOver:
+
+    choice = input(f"What would you like? {menu.get_items()}: ").lower()
+
+    if choice == "off":
+        isOver = True
+
+    elif choice == "report":
+        coffeeMaker.report()
+        moneyMachine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if(drink != None):
+            #print("Please insert coins. ")
+            if coffeeMaker.is_resource_sufficient(drink) and moneyMachine.make_payment(drink.cost):
+                coffeeMaker.make_coffee(drink)
