e58e7d5ac7cfd7d12f6edae3b809558f6d15319e
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-23 23:53:42 +0300

    made changes

---
+++ day16/day16.1.py
@@ -0,0 +1,21 @@
+"""import turtle
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("cyan4")
+turtle.forward(100)
+
+myScreen = Screen()
+print(myScreen.canvheight)
+myScreen.exitonclick()"""
+
+from prettytable import PrettyTable
+table = PrettyTable()
+print(table)
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+print(table)
