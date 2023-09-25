c1cf1f2f8ea41233ae1d1965c091ab268d2383a2
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-25 21:15:17 +0300

    made changes

--- day18/day18.2.py
+++ day18/day18.2.py
@@ -1,10 +1,15 @@
 from turtle import Turtle, Screen
+import random
 
 timmy = Turtle()
+timmy.pensize(5)
 #timmy.shape("square")
 # screen = Screen()
 
 # screen.exitonclick()
+colors = ["cornflower blue", "medium aquamarine", "yellow green", "indian red", 
+          "dark slate blue", "rosy brown", "goldenrod", "light slate gray", 
+          "powder blue", "deep pink", "crimson", "dark orange"]
 
 def drawShape(num_sides):
     for _ in range(num_sides):
@@ -12,4 +17,6 @@ def drawShape(num_sides):
         timmy.right(360/num_sides)
 
 for shape_side_n in range(3,11):
+    timmy.color(random.choice(colors))
     drawShape(shape_side_n)
+    
