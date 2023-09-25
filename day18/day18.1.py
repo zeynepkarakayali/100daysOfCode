624c9c215bb4362180d959227381e71448efd0ef
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-25 13:48:08 +0300

    added files

---
+++ day18/day18.1.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+#timmy.shape("square")
+# screen = Screen()
+
+# screen.exitonclick()
+
+for _ in range(4):
+    timmy.forward(100)
+    timmy.left(90)
