3fbdacf65f6452ca2deda21dd5fc3ca0ab1d07ef
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-26 20:25:45 +0300

    added files

--- day18/day18.5.py
+++ day18/day18.5.py
@@ -6,16 +6,22 @@ import random
 timmy = t.Turtle()
 #t.colormode(255)
 timmy.speed("fastest")
-
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
-    color = (r, g, b)
-    #return f"#{r:02X}{g:02X}{b:02X}"
-    return color
+    #color = (r, g, b)
+    return f"#{r:02X}{g:02X}{b:02X}"
+    #return color
 
-timmy.circle(100)
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        current_heading = timmy.heading()
+        timmy.setheading(current_heading + size_of_gap)
+
+draw_spirograph(3)
 
 screen = t.Screen()
 screen.exitonclick()
