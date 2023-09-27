35af5863d5762c284816be3e229f7e080600a1eb
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-27 14:42:38 +0300

    made changes

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -1,4 +1,6 @@
 from turtle import Turtle,Screen
+import time
+import snake
 
 screen = Screen()
 screen.setup(width=600, height=800)
@@ -19,8 +21,14 @@ for position in starting_positions:
 game_is_on = True
 
 while game_is_on:
-    for segment in segments:
-        segment.forward(20)
-        screen.update()
+    screen.update()
+    time.sleep(0.1)
+
+    # start len(segments) - 1 , stop 0, step -1
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(10)
 
 screen.exitonclick()
---
+++ day20/snake_game/snake.py
@@ -0,0 +1,12 @@
+class Snake():
+    def __init__(self):
+        pass
+
+    # def move(self, keyboard):
+        # if keyboard == "w":
+
+        # elif keyboard == "s":
+
+        # elif keyboard == "a":
+
+        # elif keyboard == "d":
