5c83ac9bd52908ea2ab0d372a9048aa5f79ed5a5
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-27 16:34:16 +0300

    made changes

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -1,6 +1,6 @@
 from turtle import Turtle,Screen
 import time
-import snake
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=800)
@@ -9,26 +9,19 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 starting_positions = [(0,0), (-20,0), (-40,0)]
-segments = []
+snake = Snake()
 
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.listen()
+screen.onkey("Up")
+screen.onkey("Down")
+screen.onkey("Left")
+screen.onkey("Right")
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    # start len(segments) - 1 , stop 0, step -1
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(10)
+    snake.move()
 
 screen.exitonclick()
--- day20/snake_game/snake.py
+++ day20/snake_game/snake.py
@@ -1,12 +1,25 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 10
+
 class Snake():
     def __init__(self):
-        pass
+        self.segments = []
+        self.create_snake()
 
-    # def move(self, keyboard):
-        # if keyboard == "w":
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
 
-        # elif keyboard == "s":
-
-        # elif keyboard == "a":
-
-        # elif keyboard == "d":
+    def move(self):
+        # start len(segments) - 1 , stop 0, step -1
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
