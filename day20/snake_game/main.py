dd38b5c0bfb9035cc26634a92f57f82a139413b2
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-27 11:09:41 +0300

    made changes

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -4,3 +4,12 @@ screen = Screen()
 screen.setup(width=600, height=800)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+
+starting_positions = [(0,0), (-20,0), (-40,0)]
+
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+screen.exitonclick()
