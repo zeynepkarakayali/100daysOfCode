db3d746177fff0eec067286b72f56c2b59981194
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-28 10:50:22 +0300

    made changes

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -1,21 +1,24 @@
 from turtle import Turtle,Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard 
 
 screen = Screen()
-screen.setup(width=600, height=800)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0,0), (-20,0), (-40,0)]
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey("Up")
-screen.onkey("Down")
-screen.onkey("Left")
-screen.onkey("Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
@@ -23,5 +26,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
---
+++ day20/snake_game/scoreboard.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.hideturtle()
+        self.goto(0,270)
