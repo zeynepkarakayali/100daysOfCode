f9eb7e088842a5de6a9a73c28e948bc0ff395f17
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-09-29 11:59:55 +0300

    added files

---
+++ day21/pong_game/paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+STARTING_POSITIONS = 0
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        new_ycor = self.ycor() + 20
+        self.goto(self.xcor(), new_ycor)
+
+    def go_down(self):
+        new_ycor = self.ycor() - 20
+        self.goto(self.xcor(), new_ycor)
