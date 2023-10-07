7934eb42bc877932bbfba9aeeb225309226c8dc3
zeynepkarakayali <zeynepkarakayali@gmail.com>
2023-10-07 19:12:38 +0300

    added files

---
+++ day26/day26.1.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3]
+new_numbers = [n+1 for n in numbers]
+
+print(new_numbers)
+
+name = "Angela"
+new_name = [letter for letter in name]
+print(new_name)
+
+new_nums = [n*2 for n in range(1,5)]
+print(new_nums)
